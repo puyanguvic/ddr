3aef909c8d4d76a1a312aa2d8e4977fe554a6d8d
Tianfangchang <tianfangchang@gmail.com>
2023-07-26 14:42:07 -0700

    exp1 data process
    update figure function
    process data to exp1.txt, row for rate and budget

--- infocomm2024/EXP1/EPOCH1/result/main.m
+++ infocomm2024/EXP1/EPOCH1/result/main.m
@@ -42,8 +42,27 @@ end
 % end
 % ldg = legend ("DGR", "DW", "DWE", "ECMP", "LFID", Location="bestoutside");
 
+% 打开一个文件用于输出
+fileID = fopen('exp.txt', 'w');
+
+% 设置输出格式为3位小数+制表符
+formatSpec = '%f\t';
+
+% 输出数组 A 和 B 交叉到文件
+for i = 1:50:800
+    % 输出 rate 数组的数据，每行 50
+    fprintf(fileID, formatSpec, rate(i:i+49));
+    fprintf(fileID, '\n');
+    % 输出 budget 数组的数据，每行 50
+    fprintf(fileID, formatSpec, budget(i:i+49));
+    fprintf(fileID, '\n\n');
+end
+
+% 关闭文件
+fclose(fileID);
 
 
+figure;
 for i = 1:length(topos)
     subplot (1, length(topos),i)
     for k = 1:length(prots)
@@ -51,9 +70,11 @@ for i = 1:length(topos)
         last = first + 49;
         x = budget (first:last);
         y = rate (first:last);
-        plot (x, y,'-*','LineWidth',2);
+        plot (x, y,'LineWidth',2);
+		xlabel ('Time Limit (ms)');
         hold on;
     end
 end
 
+ylabel ('Arrival Rate (%)');
 ldg = legend ("ECMP", "KSHORT", "DGR", "DDR", Location="bestoutside");
