02592f73afc84cec2e419f932f4e031dba48a794
TianfangChang <tianfangchang@gmail.com>
2023-07-25 21:48:00 -0700

    update epoch1 geant

--- infocomm2024/EXP1/EPOCH1/code/kshort.sh
+++ infocomm2024/EXP1/EPOCH1/code/kshort.sh
@@ -8,55 +8,55 @@ FILE="dgr-packet.delay"
 
 cp "${DIR}code/kshort.cc" "scratch/EPOCH1_kshort.cc"
 
-# abilene 0 --> 10 
-TOPO="abilene"
-SINK="10"
-SENDER="0"
-BEGIN=19500
-STEP=500
-for i in {1..50}
-do
-TIME=$(($i*$STEP))
-BUDGET=$(($BEGIN+$TIME))
-echo "${TOPO}${DATARATE}"
-./ns3 run "scratch/EPOCH1_kshort.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
-cp $FILE "${DIR}result/${TOPO}/KSHORT-${i}.txt"
-done
+# # abilene 0 --> 10 
+# TOPO="abilene"
+# SINK="10"
+# SENDER="0"
+# BEGIN=19500
+# STEP=500
+# for i in {1..50}
+# do
+# TIME=$(($i*$STEP))
+# BUDGET=$(($BEGIN+$TIME))
+# echo "${TOPO}${DATARATE}"
+# ./ns3 run "scratch/EPOCH1_kshort.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
+# cp $FILE "${DIR}result/${TOPO}/KSHORT-${i}.txt"
+# done
 
-# att 0 --> 17
-TOPO="att"
-SINK="17"
-SENDER="0"
-BEGIN=5000
-STEP=700
-for i in {1..50}
-do
-TIME=$(($i*$STEP))
-BUDGET=$(($BEGIN+$TIME))
-echo "${TOPO}${DATARATE}"
-./ns3 run "scratch/EPOCH1_kshort.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
-cp $FILE "${DIR}result/${TOPO}/KSHORT-${i}.txt"
-done
+# # att 0 --> 17
+# TOPO="att"
+# SINK="17"
+# SENDER="0"
+# BEGIN=5000
+# STEP=700
+# for i in {1..50}
+# do
+# TIME=$(($i*$STEP))
+# BUDGET=$(($BEGIN+$TIME))
+# echo "${TOPO}${DATARATE}"
+# ./ns3 run "scratch/EPOCH1_kshort.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
+# cp $FILE "${DIR}result/${TOPO}/KSHORT-${i}.txt"
+# done
 
-# cernet 7 --> 15
-TOPO="cernet"
-SINK="15"
-SENDER="7"
-BEGIN=5000
-STEP=600
-for i in {1..50}
-do
-TIME=$(($i*$STEP))
-BUDGET=$(($BEGIN+$TIME))
-echo "${TOPO}${DATARATE}"
-./ns3 run "scratch/EPOCH1_kshort.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
-cp $FILE "${DIR}result/${TOPO}/KSHORT-${i}.txt"
-done
+# # cernet 7 --> 15
+# TOPO="cernet"
+# SINK="15"
+# SENDER="7"
+# BEGIN=5000
+# STEP=600
+# for i in {1..50}
+# do
+# TIME=$(($i*$STEP))
+# BUDGET=$(($BEGIN+$TIME))
+# echo "${TOPO}${DATARATE}"
+# ./ns3 run "scratch/EPOCH1_kshort.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
+# cp $FILE "${DIR}result/${TOPO}/KSHORT-${i}.txt"
+# done
 
-# geant 19 --> 8
+# geant 16 --> 14
 TOPO="geant"
-SINK="7"
-SENDER="21"
+SINK="16"
+SENDER="14"
 BEGIN=20000
 STEP=500
 for i in {1..50}
