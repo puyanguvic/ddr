ffda69f7c21a9ad1938ced43dd99e50c138a5f1b
TianfangChang <tianfangchang@gmail.com>
2023-07-25 21:48:49 -0700

    update

--- infocomm2024/EXP1/EPOCH1/code/ddr.sh
+++ infocomm2024/EXP1/EPOCH1/code/ddr.sh
@@ -8,50 +8,50 @@ FILE="dgr-packet.delay"
 
 cp "${DIR}code/ddr.cc" "scratch/EPOCH1_ddr.cc"
 
-# # abilene 0 --> 10
-# TOPO="abilene"
-# SINK="10"
-# SENDER="0"
-# BEGIN=20000
-# STEP=500
-# for i in {1..50}
-# do
-# TIME=$(($i*$STEP))
-# BUDGET=$(($BEGIN+$TIME))
-# echo "${TOPO}${DATARATE}"
-# ./ns3 run "scratch/EPOCH1_ddr.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
-# cp $FILE "${DIR}result/${TOPO}/DDR-${i}.txt"
-# done
+# abilene 0 --> 10
+TOPO="abilene"
+SINK="10"
+SENDER="0"
+BEGIN=20000
+STEP=500
+for i in {1..50}
+do
+TIME=$(($i*$STEP))
+BUDGET=$(($BEGIN+$TIME))
+echo "${TOPO}${DATARATE}"
+./ns3 run "scratch/EPOCH1_ddr.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
+cp $FILE "${DIR}result/${TOPO}/DDR-${i}.txt"
+done
 
-# # att 20 --> 14
-# TOPO="att"
-# SINK="14"
-# SENDER="20"
-# BEGIN=5000
-# STEP=700
-# for i in {1..50}
-# do
-# TIME=$(($i*$STEP))
-# BUDGET=$(($BEGIN+$TIME))
-# echo "${TOPO}${DATARATE}"
-# ./ns3 run "scratch/EPOCH1_ddr.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
-# cp $FILE "${DIR}result/${TOPO}/DDR-${i}.txt"
-# done
+# att 20 --> 14
+TOPO="att"
+SINK="14"
+SENDER="20"
+BEGIN=5000
+STEP=700
+for i in {1..50}
+do
+TIME=$(($i*$STEP))
+BUDGET=$(($BEGIN+$TIME))
+echo "${TOPO}${DATARATE}"
+./ns3 run "scratch/EPOCH1_ddr.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
+cp $FILE "${DIR}result/${TOPO}/DDR-${i}.txt"
+done
 
-# # cernet 7 --> 13
-# TOPO="cernet"
-# SINK="13"
-# SENDER="7"
-# BEGIN=5000
-# STEP=600
-# for i in {1..50}
-# do
-# TIME=$(($i*$STEP))
-# BUDGET=$(($BEGIN+$TIME))
-# echo "${TOPO}${DATARATE}"
-# ./ns3 run "scratch/EPOCH1_ddr.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
-# cp $FILE "${DIR}result/${TOPO}/DDR-${i}.txt"
-# done
+# cernet 7 --> 13
+TOPO="cernet"
+SINK="13"
+SENDER="7"
+BEGIN=5000
+STEP=600
+for i in {1..50}
+do
+TIME=$(($i*$STEP))
+BUDGET=$(($BEGIN+$TIME))
+echo "${TOPO}${DATARATE}"
+./ns3 run "scratch/EPOCH1_ddr.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
+cp $FILE "${DIR}result/${TOPO}/DDR-${i}.txt"
+done
 
 # geant 16 --> 14
 TOPO="geant"
--- infocomm2024/EXP1/EPOCH1/code/ospf.sh
+++ infocomm2024/EXP1/EPOCH1/code/ospf.sh
@@ -8,50 +8,50 @@ FILE="dgr-packet.delay"
 
 cp "${DIR}code/ospf.cc" "scratch/EPOCH1_ospf.cc"
 
-# # abilene 0 --> 10
-# TOPO="abilene"
-# SINK="10"
-# SENDER="0"
-# BEGIN=19500
-# STEP=500
-# for i in {1..50}
-# do
-# TIME=$(($i*$STEP))
-# BUDGET=$(($BEGIN+$TIME))
-# echo "${TOPO}${DATARATE}"
-# ./ns3 run "scratch/EPOCH1_ospf.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
-# cp $FILE "${DIR}result/${TOPO}/OSPF-${i}.txt"
-# done
+# abilene 0 --> 10
+TOPO="abilene"
+SINK="10"
+SENDER="0"
+BEGIN=19500
+STEP=500
+for i in {1..50}
+do
+TIME=$(($i*$STEP))
+BUDGET=$(($BEGIN+$TIME))
+echo "${TOPO}${DATARATE}"
+./ns3 run "scratch/EPOCH1_ospf.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
+cp $FILE "${DIR}result/${TOPO}/OSPF-${i}.txt"
+done
 
-# # att 20 --> 14
-# TOPO="att"
-# SINK="14"
-# SENDER="20"
-# BEGIN=5000
-# STEP=700
-# for i in {1..50}
-# do
-# TIME=$(($i*$STEP))
-# BUDGET=$(($BEGIN+$TIME))
-# echo "${TOPO}${DATARATE}"
-# ./ns3 run "scratch/EPOCH1_ospf.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
-# cp $FILE "${DIR}result/${TOPO}/OSPF-${i}.txt"
-# done
+# att 20 --> 14
+TOPO="att"
+SINK="14"
+SENDER="20"
+BEGIN=5000
+STEP=700
+for i in {1..50}
+do
+TIME=$(($i*$STEP))
+BUDGET=$(($BEGIN+$TIME))
+echo "${TOPO}${DATARATE}"
+./ns3 run "scratch/EPOCH1_ospf.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
+cp $FILE "${DIR}result/${TOPO}/OSPF-${i}.txt"
+done
 
-# # cernet 7 --> 13
-# TOPO="cernet"
-# SINK="13"
-# SENDER="7"
-# BEGIN=5000
-# STEP=600
-# for i in {1..50}
-# do
-# TIME=$(($i*$STEP))
-# BUDGET=$(($BEGIN+$TIME))
-# echo "${TOPO}${DATARATE}"
-# ./ns3 run "scratch/EPOCH1_ospf.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
-# cp $FILE "${DIR}result/${TOPO}/OSPF-${i}.txt"
-# done
+# cernet 7 --> 13
+TOPO="cernet"
+SINK="13"
+SENDER="7"
+BEGIN=5000
+STEP=600
+for i in {1..50}
+do
+TIME=$(($i*$STEP))
+BUDGET=$(($BEGIN+$TIME))
+echo "${TOPO}${DATARATE}"
+./ns3 run "scratch/EPOCH1_ospf.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
+cp $FILE "${DIR}result/${TOPO}/OSPF-${i}.txt"
+done
 
 # geant 16 --> 14
 TOPO="geant"
