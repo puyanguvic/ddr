236971ddc3281031e086b54702479450bbf2c963
Tianfangchang <tianfangchang@gmail.com>
2023-07-26 16:23:03 -0700

    update

--- infocomm2024/EXP1/EPOCH2/result/main.m
+++ infocomm2024/EXP1/EPOCH2/result/main.m
@@ -24,42 +24,40 @@ for i = 1:length(topos)
     budget = [budget; topo_budget];
 end
 
-
-% 
-% for i = 1:length(topos)
-%     subplot (1,length(topos),i)
-%     plot (x,d((i-1)*length(prots) + 1,:), '-s', 'LineWidth',2, 'MarkerSize',8); 
-%     hold on;
-%     plot (x,d((i-1)*length(prots) + 2,:), '-o', 'LineWidth',2, 'MarkerSize',8); 
-%     hold on;
-%     plot (x,d((i-1)*length(prots) + 3,:), '-^', 'LineWidth',2, 'MarkerSize',8); 
-%     hold on;
-%     plot (x,d((i-1)*length(prots) + 4,:), '-d', 'LineWidth',2, 'MarkerSize',8); 
-%     hold on;
-%     plot (x,d((i-1)*length(prots) + 5,:), '-x', 'LineWidth',2, 'MarkerSize',8); 
-%     hold on;
-%     axis([0,10.5,0,300])
-% end
-% ldg = legend ("DGR", "DW", "DWE", "ECMP", "LFID", Location="bestoutside");
-
-% 打开一个文件用于输出
-fileID = fopen('data.txt', 'w');
-
-% 设置输出格式为小数+制表符
+% output data
+fileoutput = fopen('data.txt', 'w');
 formatSpec = '%f\t';
 
-% 输出数组 A 和 B 交叉到文件
 for i = 1:50:800
-    % 输出 rate 数组的数据，每行 50
-    fprintf(fileID, formatSpec, rate(i:i+49));
-    fprintf(fileID, '\n');
-    % 输出 budget 数组的数据，每行 50
-    fprintf(fileID, formatSpec, budget(i:i+49));
-    fprintf(fileID, '\n\n');
+    fprintf(fileoutput, formatSpec, rate(i:i+49));
+    fprintf(fileoutput, '\n');
+    fprintf(fileoutput, formatSpec, budget(i:i+49));
+    fprintf(fileoutput, '\n');
 end
 
-% 关闭文件
-fclose(fileID);
+fclose(fileoutput);
+
+% input data
+fileinput = fopen('data.txt', 'r');
+formatSpec = '%f';
+data = fscanf(fileinput, '%f');
+fclose(fileinput);
+
+for i = 1:50:800
+    newrate(i:i+49) = data(2*i-1:2*i-1+49);
+    newbudget(i:i+49) = data(2*i+49:2*i+49+49);
+end
+
+newrate = newrate' ;
+newbudget = newbudget' ;
+
+% result = isequal(newrate, rate);
+% 
+% if result
+%     disp('数组A和B相同');
+% else
+%     disp('数组A和B不相同');
+% end
 
 
 figure;
@@ -68,8 +66,8 @@ for i = 1:length(topos)
     for k = 1:length(prots)
         first = (i-1)*200 + (k-1)*50 + 1;
         last = first + 49;
-        x = budget (first:last);
-        y = rate (first:last);
+        x = newbudget (first:last);
+        y = newrate (first:last);
         plot (x, y,'LineWidth',2);
 		xlabel ('Time Limit (ms)');
         hold on;
@@ -78,3 +76,4 @@ end
 
 ylabel ('Arrival Rate (%)');
 ldg = legend ("ECMP", "KSHORT", "DGR", "DDR", Location="bestoutside");
+
