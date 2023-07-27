adc97652b5cb6ef931e4d2834a209b222d119076
TianfangChang <tianfangchang@gmail.com>
2023-07-26 23:11:09 -0700

    update

--- infocomm2024/EXP1/EPOCH2/code/exp1-2.sh
+++ infocomm2024/EXP1/EPOCH2/code/exp1-2.sh
@@ -3,7 +3,7 @@
 echo "EXP_1 Epoch_2"
 echo ""
 
-# ./contrib/dgrv2/infocomm2024/EXP1/EPOCH2/code/ospf.sh 
-# ./contrib/dgrv2/infocomm2024/EXP1/EPOCH2/code/kshort.sh 
+./contrib/dgrv2/infocomm2024/EXP1/EPOCH2/code/ospf.sh 
+./contrib/dgrv2/infocomm2024/EXP1/EPOCH2/code/kshort.sh 
 ./contrib/dgrv2/infocomm2024/EXP1/EPOCH2/code/dgr.sh 
 ./contrib/dgrv2/infocomm2024/EXP1/EPOCH2/code/ddr.sh 
