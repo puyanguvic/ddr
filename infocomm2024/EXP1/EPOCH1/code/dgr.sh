47e3895898953940aec36f58b3fbdde4ba75c9ae
TianfangChang <tianfangchang@gmail.com>
2023-07-26 17:45:17 -0700

    update epoch 1

--- infocomm2024/EXP1/EPOCH1/code/dgr.sh
+++ infocomm2024/EXP1/EPOCH1/code/dgr.sh
@@ -8,57 +8,57 @@ FILE="dgr-packet.delay"
 
 cp "${DIR}code/dgr.cc" "scratch/EPOCH1_dgr.cc"
 
-# abilene 0 --> 10
-TOPO="abilene"
-SINK="10"
-SENDER="0"
-BEGIN=19500
-STEP=500
-for i in {1..50}
-do
-TIME=$(($i*$STEP))
-BUDGET=$(($BEGIN+$TIME))
-echo "${TOPO}${DATARATE}"
-./ns3 run "scratch/EPOCH1_dgr.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
-cp $FILE "${DIR}result/${TOPO}/DGR-${i}.txt"
-done
+# # abilene 0 --> 10
+# TOPO="abilene"
+# SINK="10"
+# SENDER="0"
+# BEGIN=19500
+# STEP=500
+# for i in {1..50}
+# do
+# TIME=$(($i*$STEP))
+# BUDGET=$(($BEGIN+$TIME))
+# echo "${TOPO}${DATARATE}"
+# ./ns3 run "scratch/EPOCH1_dgr.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
+# cp $FILE "${DIR}result/${TOPO}/DGR-${i}.txt"
+# done
 
-# att 20 --> 14
-TOPO="att"
-SINK="14"
-SENDER="20"
-BEGIN=5000
-STEP=700
-for i in {1..50}
-do
-TIME=$(($i*$STEP))
-BUDGET=$(($BEGIN+$TIME))
-echo "${TOPO}${DATARATE}"
-./ns3 run "scratch/EPOCH1_dgr.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
-cp $FILE "${DIR}result/${TOPO}/DGR-${i}.txt"
-done
+# # att 20 --> 14
+# TOPO="att"
+# SINK="14"
+# SENDER="20"
+# BEGIN=5000
+# STEP=700
+# for i in {1..50}
+# do
+# TIME=$(($i*$STEP))
+# BUDGET=$(($BEGIN+$TIME))
+# echo "${TOPO}${DATARATE}"
+# ./ns3 run "scratch/EPOCH1_dgr.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
+# cp $FILE "${DIR}result/${TOPO}/DGR-${i}.txt"
+# done
 
-# cernet 7 --> 13
-TOPO="cernet"
-SINK="13"
-SENDER="7"
-BEGIN=5000
-STEP=600
-for i in {1..50}
-do
-TIME=$(($i*$STEP))
-BUDGET=$(($BEGIN+$TIME))
-echo "${TOPO}${DATARATE}"
-./ns3 run "scratch/EPOCH1_dgr.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
-cp $FILE "${DIR}result/${TOPO}/DGR-${i}.txt"
-done
+# # cernet 7 --> 13
+# TOPO="cernet"
+# SINK="13"
+# SENDER="7"
+# BEGIN=5000
+# STEP=600
+# for i in {1..50}
+# do
+# TIME=$(($i*$STEP))
+# BUDGET=$(($BEGIN+$TIME))
+# echo "${TOPO}${DATARATE}"
+# ./ns3 run "scratch/EPOCH1_dgr.cc --topo=${TOPO} --sink=${SINK} --sender=${SENDER} --budget=${BUDGET}"
+# cp $FILE "${DIR}result/${TOPO}/DGR-${i}.txt"
+# done
 
 # geant 14 --> 16
 TOPO="geant"
 SINK="16"
 SENDER="14"
 BEGIN=20000
-STEP=500
+STEP=700
 for i in {1..50}
 do
 TIME=$(($i*$STEP))
