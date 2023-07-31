33a8bd06405ef0fbcf1039a6873d180191069fca
pu <pu@w206-87-183-112.wireless.uvic.ca>
2023-07-31 11:36:03 -0700

    update

--- infocomm2024/EXP2/tcp/result/untitled.m
+++ infocomm2024/EXP2/tcp/result/untitled.m
@@ -1,6 +1,60 @@
+% clc
+% clear
+% close all
+% tcp = ["Bbr" "Cubic" "Dctcp" "Vegas"];
+% prots = ["OSPF" "DDR"];
+% throughput = [];
+% time = [];
+% for i = 1:length(prots)
+%     for k = 1:length(tcp)
+%         filename = strcat(prots{i}, "-Tcp", tcp{k}, "-throughput.txt");
+%         data = load('DDR-TcpCubic-throughput.txt');
+%         X = data(:,1);
+%         Y = data(:,2);
+%         throughput = [throughput;Y];
+%         time = [time;X];
+%     end
+% end
+% 
+% figure;
+% 
+% 
+% for i = 1:length(prots)
+%     for k = 1:length(tcp)
+%         first = (i-1)*200 + (k-1)*50 + 1;
+%         last = first + 49;
+%         x = newbudget (first:last);
+%         y = newrate (first:last);
+%         plot (x, y,'LineWidth',2);
+%         ylabel ('On-time Arrival Rate (%)');
+% 		xlabel ('Time Limit (ms)');
+%         ylim([-0.05 1.05])
+%         grid on;
+%         hold on;
+%     end
+% end
+% 
+% 
+% % legend('Orientation','horizontal');
+% ldg = legend ("ECMP", "LFID", "DGR", "DDR", Location="north");
+% set(ldg,'Orientation','horizon')
+% set(ldg,'Box','off');
+% set(gca, 'FontSize',11);
+
+
+
+
+
+
+
+
+
+
+
+
 clear;
 
-data = load('TcpCubic-cwnd.txt');
+data = load('DDR-TcpCubic-throughput.txt');
 X = data(:,1);
 Y = data(:,2);
 
@@ -9,7 +63,7 @@ hold on;
 
 clear;
 
-data = load('TcpBbr-cwnd.txt');
+data = load('DDR-TcpBbr-throughput.txt');
 X = data(:,1);
 Y = data(:,2);
 
@@ -18,7 +72,7 @@ hold on;
 
 clear;
 
-data = load('TcpVegas-cwnd.txt');
+data = load('DDR-TcpVegas-throughput.txt');
 X = data(:,1);
 Y = data(:,2);
 
@@ -27,7 +81,7 @@ hold on;
 
 clear;
 
-data = load('TcpDctcp-cwnd.txt');
+data = load('DDR-TcpDctcp-throughput.txt');
 X = data(:,1);
 Y = data(:,2);
 
